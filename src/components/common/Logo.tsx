6c57203a936f59d756db93b8ee6eaa67b0285b18
Filip Jevtic <fmj.jevtic@gmail.com>
2026-05-11 16:56:06 +0200

    use png logo, remove icon generation from ci

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 interface LogoProps {
   size?: number
   className?: string
@@ -5,22 +7,14 @@ interface LogoProps {
 
 export function Logo({ size = 32, className = '' }: LogoProps) {
   return (
-    <svg
+    <Image
+      src="/icons/icon-192x192.png"
+      alt="MoonAuth"
       width={size}
       height={size}
-      viewBox="0 0 100 100"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
-      aria-label="MoonAuth logo"
-    >
-      <rect width="100" height="100" fill="#000000" />
-      <g transform="rotate(15 50 50)">
-        <circle cx="42" cy="50" r="26" fill="white" />
-        <circle cx="57" cy="50" r="22" fill="#000000" />
-        <circle cx="42" cy="50" r="26" fill="white" fillOpacity="0.03" />
-      </g>
-    </svg>
+      className={`rounded-xl ${className}`}
+      priority
+    />
   )
 }
 
